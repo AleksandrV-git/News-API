7db9bef411e1df0b94e44edd9ad5beebb4c8ed8a
AleksandrV <kolomel@yandex.ru>
2020-12-07 14:52:22 +0300

    fix login controller

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true }).end();
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true }).send({data: { name: user.name }});
     })
     .catch(next);// ошибка 401 обрабатывается в модели
 };
